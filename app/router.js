e9ab0e49f993da9b54a829f5b7ab9254f2b9850e
Reed Lambier <rlambier46@gmail.com>
2016-06-08 10:03:07 -0700

    Add working update

---
+++ app/router.js
@@ -0,0 +1,15 @@
+import Ember from 'ember';
+import config from './config/environment';
+
+const Router = Ember.Router.extend({
+  location: config.locationType
+});
+
+Router.map(function() {
+  this.route('about');
+  this.route('contact');
+  this.route('anouncements');
+  this.route('rental', {path: '/rental/:rental_id'});
+});
+
+export default Router;
