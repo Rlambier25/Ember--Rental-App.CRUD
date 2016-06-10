87411ca0005c64c3df3d1132bcf0300da8d68b09
Reed Lambier <rlambier46@gmail.com>
2016-06-09 18:56:57 -0700

    added the deletion of rentals along with the deletion of cities

--- app/routes/city.js
+++ app/routes/city.js
@@ -5,7 +5,7 @@ export default Ember.Route.extend({
   model(params) {
     return this.store.findRecord('city', params.city_id);
   },
-  
+
   actions: {
     save3(params) {
       var newRental = this.store.createRecord('rental', params);
@@ -15,6 +15,15 @@ export default Ember.Route.extend({
         return city.save();
       });
       this.transitionTo('city', params.city);
+    },
+    destroyCity(city) {
+      var rental_deletions = city.get('rentals').map(function(rental) {
+        return rental.destroyRecord();
+      });
+      Ember.RSVP.all(rental_deletions).then(function() {
+        return city.destroyRecord();
+      });
+      this.transitionTo('index');
     }
   }
 });
