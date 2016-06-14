8353af47314a2ef65e3ed9322a6a53005658694c
Reed Lambier <rlambier46@gmail.com>
2016-06-13 18:18:16 -0700

    added popularity and dollar sign glyphicons depending on price and number of rentals. Also added a various sort feature.

--- app/components/city-tile.js
+++ app/components/city-tile.js
@@ -1,6 +1,14 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
+
+  fullLocation: Ember.computed('city.name', 'city.country', function() {
+  return this.get('city.name') + ', ' + this.get('city.country');
+}),
+
+sortBy: ['cost:asc'],
+  sortedRentals: Ember.computed.sort('city.rentals', 'sortBy'),
+
   actions: {
     destroyCity(city) {
       if (confirm('Are you sure you want to delete this city?')) {
--- app/components/new-rental.js
+++ app/components/new-rental.js
@@ -14,6 +14,7 @@ export default Ember.Component.extend({
         type: this.get('type'),
         image: this.get('image'),
         bedrooms: this.get('bedrooms'),
+        cost: parseInt(this.get('cost'))
       };
       this.set('addNewRental', false);
       this.sendAction('save2', params);
